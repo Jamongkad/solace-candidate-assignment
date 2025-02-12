87acc91eaa52cea753d60acc8f6cb1eee030aa56
Mathew Wong <Jamongkad@users.noreply.github.com>
2025-02-11 17:02:08 -0800

    Adding API unit tests.

---
+++ src/app/api/advocates/__tests__/route.test.js
@@ -0,0 +1,59 @@
+/**
+ * @jest-environment node
+ */
+import { desc } from 'drizzle-orm';
+import { GET } from '../route.ts'
+import { AdvocateService } from '../service/AdvocateService.ts'; // Adjust the import path as necessary
+import { describe } from 'node:test';
+
+let mockFind = jest.fn();
+jest.mock('../service/AdvocateService', () => ({
+  AdvocateService: jest.fn().mockImplementation(() => ({
+    find: mockFind
+  }))
+})); // Mock the AdvocateService module
+
+describe('GET /advocates', () => {
+  it('should return a list of advocates', async () => {
+    const mockAdvocates = [
+      { id: 1, name: 'Advocate 1' },
+      { id: 2, name: 'Advocate 2' },
+    ];
+
+    mockFind.mockResolvedValue({
+      data: mockAdvocates,
+      count: 10
+    });
+
+    const requestObj = {
+      nextUrl: {
+        searchParams: new URLSearchParams({ limit: '10', offset: '4' }),
+      },
+    }
+
+    const response = await GET(requestObj);
+    const body = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(body).toEqual({
+      data: mockAdvocates,
+      count: 10
+    });
+  });
+
+  it('should throw error if AdvocateService.find() throws error', async () => {
+    mockFind.mockRejectedValue(new Error('Internal Server Error'));
+
+    const requestObj = {
+      nextUrl: {
+        searchParams: new URLSearchParams({ limit: '10', offset: '4' }),
+      },
+    }
+
+    try {
+      await GET(requestObj);
+    } catch (error) {
+      expect(error.message).toBe('Internal Server Error');
+    }
+  });
+});
--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,7 +1,5 @@
-import db from "../../../db";
-import { asc, count } from 'drizzle-orm';
-import { advocates } from "../../../db/schema";
 import { type NextRequest } from 'next/server'
+import { AdvocateService } from "./service/AdvocateService";
 
 export async function GET(req: NextRequest) {
 	const searchParams = req.nextUrl.searchParams;
@@ -9,12 +7,13 @@ export async function GET(req: NextRequest) {
 	const limit = searchParams.get('limit');
 	const offset = searchParams.get('offset');
 
-	const data = await db.select().from(advocates)
-		.orderBy(asc(advocates.id))
-		.limit(Number(limit)) // the number of rows to return
-		.offset(Number(offset)); // the number of rows to skip
+	try {
+		const advocateService = new AdvocateService();
+		const { data, count } = await advocateService.find(Number(limit), Number(offset));
 
-	const resultCount = await db.select({ count: count() }).from(advocates);
-	
-	return Response.json({ data, count: resultCount[0].count });
+		return Response.json({ data, count });
+
+	} catch (error) {
+		return Response.error();
+	}
 }
